99458b6dd3267e36d8a5278f38c519f2725bff41
dylanruddy <95940291+dylanruddy@users.noreply.github.com>
2024-07-12 22:13:48 -0400

    Revert Back To Previous Code
    The cosolidated code didn't work.

--- Math Quiz.cpp
+++ Math Quiz.cpp
@@ -6,10 +6,9 @@ using namespace std;
 
 int main() {
     int testscore = 0;
-    const int numQuestions = 10;
-    int answers[numQuestions] = {0}; // Array to store user answers
+    int question1answer, question2answer, question3answer, question4answer, question5answer, question6answer, question7answer, question8answer, question9answer, question10answer;
 
-    string introduction = "Welcome to the Math Quiz! This is a 10 question test, your score will appear at the end. Press Enter to begin!";
+    string introduction = "Welcome to the Math Quiz! This is a 4 question test, your score will appear at the end. Press Enter to begin!";
     cout << introduction << endl;
     cin.get();  // Wait for user to press Enter
 
@@ -26,22 +25,102 @@ int main() {
         {"What is 534 + 50?", {"A. 584", "B. 585", "C. 586", "D. 587"}}
     };
 
-    // Iterate over each question
-    for (int i = 0; i < numQuestions; ++i) {
-        cout << questions[i].first << endl;
-        for (const string& option : questions[i].second) {
-            cout << option << endl;
-        }
-        cout << "Enter your answer (1-4): ";
-        cin >> answers[i];
+    // Question 1
+    cout << questions[0].first << endl;
+    for (const string& option : questions[0].second) {
+        cout << option << endl;
+    }
+    cout << "Enter your answer (1-4): ";
+    cin >> question1answer;
+
+    // Question 2
+    cout << questions[1].first << endl;
+    for (const string& option : questions[1].second) {
+        cout << option << endl;
+    }
+    cout << "Enter your answer (1-4): ";
+    cin >> question2answer;
+
+    // Question 3
+    cout << questions[2].first << endl;
+    for (const string& option : questions[2].second) {
+        cout << option << endl;
+    }
+    cout << "Enter your answer (1-4): ";
+    cin >> question3answer;
+
+    // Question 4
+    cout << questions[3].first << endl;
+    for (const string& option : questions[3].second) {
+        cout << option << endl;
+    }
+    cout << "Enter your answer (1-4): ";
+    cin >> question4answer;
+
+    // Question 5
+    cout << questions[4].first << endl;
+    for (const string& option : questions[4].second) {
+        cout << option << endl;
+    }
+    cout << "Enter your answer (1-4): ";
+    cin >> question5answer;
+
+    // Question 6
+    cout << questions[5].first << endl;
+    for (const string& option : questions[5].second){
+        cout << option << endl;
+    }
+    
+    cout << "Enter your answer (1-4): ";
+    cin >> question6answer;
+
+    // Question 7
+    cout << questions[6].first << endl;
+    for (const string& option : questions[6].second){
+        cout << option << endl;
     }
 
-    // Calculate test score
-    for (int i = 0; i < numQuestions; ++i) {
-        if (answers[i] == 4) { // Assuming correct answer is always D (option 4)
-            testscore++;
-        }
+    cout << "Enter your answer (1-4): ";
+    cin >> question7answer;
+
+    // Question 8
+    cout << questions[7].first;
+    for (const string& option : questions[7].second){
+        cout << option << endl;
+    }  
+
+    cout << "Enter your answer (1-4): ";
+    cin >> question8answer;
+
+    // Question 9
+    cout << questions[8].first;
+    for (const string& option : questions[8].second){
+        cout << option << endl;
+    }  
+
+    cout << "Enter your answer (1-4): ";
+    cin >> question9answer;
+
+    // Question 10
+    cout << questions[9].first << endl;
+    for (const string& option : questions[9].second){
+        cout << option << endl;
     }
+
+    cout << "Enter your answer (1-4): ";
+    cin >> question10answer;
+
+    // Test score calculations
+    if (question1answer == 4) testscore++;
+    if (question2answer == 2) testscore++;
+    if (question3answer == 3) testscore++;
+    if (question4answer == 4) testscore++;
+    if (question5answer == 4) testscore++;
+    if (question6answer == 1) testscore++;
+    if (question7answer == 1) testscore++;
+    if (question8answer == 1) testscore++;
+    if (question9answer == 1) testscore++;
+    if (question10answer == 1) testscore++;
 
     cout << "Your test score is " << testscore << endl;
 
